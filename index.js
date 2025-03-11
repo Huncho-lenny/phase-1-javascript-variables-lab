cd71d7c40756920df3f4936fd3c5c2efb9f17390
lenny <lenny.kimanthi@student.moringaschool.com>
2025-03-11 21:47:08 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Code your solution in this file!
+const companyName = "Scuber"; // should be a constant
+
+let mostProfitableNeighborhood = "Chelsea"; // should be declared with let
+let companyCeo = "Susan Smith"; // should be declared with let
